5da0a0dc2c4466f942349a39f8b9bfb6b0274947
Xie Yuheng <xyheme@gmail.com>
2019-11-30 18:24:59 +0800

    up

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const http = require("http")
 
 const ms = require("ms")
 
-const env = require("./src/env.js")
-// const db = require("./src/db.js")
-const app = require("./src/app.js")
+const env = require("./lib/env.js")
+const app = require("./lib/app.js")
 
 // setInterval(() => {
 //   db.cleanUp()
